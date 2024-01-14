5517663b58252d56bdff74ce00234c8afa1cc679
Oliver Lorenz <Oliver Lorenz>
2024-01-14 12:18:25 +0100

    37c3 save

--- src/main.cpp
+++ src/main.cpp
@@ -4,82 +4,148 @@
 #include "GUI_Paint.h"
 #include "ImageData.h"
 #include <stdlib.h>
+#include "WiFi.h"
+#include <HTTPClient.h>
+
+const char *ssid = "37C3-open";
+// https://cloud.chemnitz.freifunk.net/s/wXSZokxmCq8WCPs/download/test_8bit_sw.bmp
+String HOST_NAME = "https://cloud.chemnitz.freifunk.net";              // CHANGE IT
+String PATH_NAME = "/s/FLqfzPxSDjD7Hpx/download/test_8bit_sw_640.bmp"; // CHANGE IT
+
+HTTPClient http;
+
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+const unsigned char *doHttpRequest()
+{
+  Serial.println("Start request");
+  http.begin(HOST_NAME + PATH_NAME); // HTTP
+
+  int httpCode = http.GET();
+  Serial.print("Http Code: ");
+  Serial.println(httpCode);
+
+  if (httpCode > 0)
+  {
+    // file found at server
+    if (httpCode == HTTP_CODE_OK)
+    {
+      int size = http.getSize();
+      Serial.print("Response Payload Size: ");
+      Serial.println(size);
+
+      String payload = http.getString();
+
+      // Length (with one extra character for the null terminator)
+      unsigned int str_len = payload.length();
+      Serial.print("Response Payload length: ");
+      Serial.println(str_len);
+      if (str_len < 10000)
+      {
+        Serial.print("Payload zu klein!?");
+        //  Panic!
+        while (1)
+          ;
+      }
+
+      // Serial.println("toCharArray..");
+      // // char char_array[str_len];
+      // const int zu_viel = 10000; // 125 lt. HEX Dump
+      // String payload_nach_header = payload.substring(10000, 20000);
+      unsigned char char_array_ohne_header[10000]; // str_len - zu_viel];
+      return char_array_ohne_header;
+      // payload_nach_header.toCharArray(char_array_ohne_header, 10000); // str_len);
+
+      // 800 x 480 = 384.000
+      // erste 40 Byte sind BMP Header
+      // 800 x 480 = 384.000
+      // gImage_7in5 ist 30720
+      // haben gerade    30850
+      // sind also         130 zu viel
+
+      // Serial.println("Caste dein erstes Kind auf Int..");
+      // unsigned char unsig_char_array_ohne_header[10000]; // str_len - zu_viel];
+      // for (int i = 0; i < 10000; i++)
+      // {
+      //   unsig_char_array_ohne_header[i] = (unsigned char)char_array_ohne_header[i];
+      // }
+
+      Serial.println("Write to display");
+      // Paint_DrawBitMap(gImage_7in5);
+      // Paint_DrawBitMap((unsigned char *) char_array_ohne_header);
+      Serial.println("request done");
+    }
+    else
+    {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET/POST... code: %d\n", httpCode);
+    }
+  }
+  else
+  {
+    Serial.printf("[HTTP] GET/POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+}
 
 /* Entry point ----------------------------------------------------------------*/
 void setup()
 {
-  printf("EPD_7IN5_test Demo\r\n");
-  DEV_Module_Init();
+  Serial.begin(115200);
+  initWiFi();
+  DEV_Delay_ms(500);
 
-  printf("e-Paper Init and Clear...\r\n");
+  DEV_Module_Init();
+  Serial.println("Init EINK");
   EPD_7IN5_Init();
   EPD_7IN5_Clear();
   DEV_Delay_ms(500);
 
-  // Create a new image cache
+  Serial.println("Create a new image cache");
   UBYTE *BlackImage;
   /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
   UWORD Imagesize = ((EPD_7IN5_WIDTH % 8 == 0) ? (EPD_7IN5_WIDTH / 8) : (EPD_7IN5_WIDTH / 8 + 1)) * EPD_7IN5_HEIGHT;
   if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL)
   {
-    printf("Failed to apply for black memory...\r\n");
+    Serial.println("Failed to apply for black memory...");
     while (1)
       ;
   }
-  printf("Paint_NewImage\r\n");
+  Serial.println("Paint_NewImage");
+  // START BLOCK
   Paint_NewImage(BlackImage, EPD_7IN5_WIDTH, EPD_7IN5_HEIGHT, 0, WHITE);
 
-#if 1 // show image for array
-  printf("show image for array\r\n");
+  Serial.println("show image for array");
   Paint_SelectImage(BlackImage);
   Paint_Clear(WHITE);
-  Paint_DrawBitMap(gImage_7in5);
+  // Paint_DrawBitMap(gImage_7in5);
+  Paint_DrawBitMap(doHttpRequest());
+  Serial.println("EPD_7IN5_Display BlackImage");
   EPD_7IN5_Display(BlackImage);
+  // END BLOCK
   DEV_Delay_ms(500);
-#endif
 
-#if 1 // Drawing on the image
-  // 1.Select Image
-  printf("SelectImage:BlackImage\r\n");
-  Paint_SelectImage(BlackImage);
-  Paint_Clear(WHITE);
-
-  // 2.Drawing on the image
-  printf("Drawing:BlackImage\r\n");
-  Paint_DrawPoint(10, 80, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-  Paint_DrawPoint(10, 90, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-  Paint_DrawPoint(10, 100, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-  Paint_DrawLine(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-  Paint_DrawLine(70, 70, 20, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-  Paint_DrawRectangle(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-  Paint_DrawRectangle(80, 70, 130, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-  Paint_DrawCircle(45, 95, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-  Paint_DrawCircle(105, 95, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-  Paint_DrawLine(85, 95, 125, 95, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-  Paint_DrawLine(105, 75, 105, 115, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-  Paint_DrawString_EN(10, 0, "waveshare", &Font16, BLACK, WHITE);
-  Paint_DrawString_EN(10, 20, "hello world", &Font12, WHITE, BLACK);
-  Paint_DrawNum(10, 33, 123456789, &Font12, BLACK, WHITE);
-  Paint_DrawNum(10, 50, 987654321, &Font16, WHITE, BLACK);
-  Paint_DrawString_CN(130, 0, " 你好abc", &Font12CN, BLACK, WHITE);
-  Paint_DrawString_CN(130, 20, "微雪电子", &Font24CN, WHITE, BLACK);
-
-  printf("EPD_Display\r\n");
-  EPD_7IN5_Display(BlackImage);
-  DEV_Delay_ms(2000);
-#endif
-
-  printf("Clear...\r\n");
-  EPD_7IN5_Clear();
-
-  printf("Goto Sleep...\r\n");
-  EPD_7IN5_Sleep();
-  free(BlackImage);
-  BlackImage = NULL;
+  // Serial.println("doHttpRequest");
+  // Paint_SelectImage(BlackImage);
+  // Paint_Clear(WHITE);
 }
 
 /* The main loop -------------------------------------------------------------*/
 void loop()
 {
-  //
+  // Paint_DrawBitMap(gImage_7in5);
+  //  reset nach 1 Min
+  // DEV_Delay_ms(60 * 1000);
+  // TODO Reset ..
+  // ESP.restart();
 }
